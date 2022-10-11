5701636c179de37ec4abcdc8fe1bb8f4de32d909
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2022-10-11 21:03:57 +0200

    first commit

---
+++ untitled1/src/request.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class request {
+    public  static  void getPrint(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("veuillez entrer un nombre");
+        Integer nbr= scan.nextInt();
+        System.out.println("vous avez choisis le nombre" + " " + nbr);
+        if (nbr %2==0){
+            System.out.println("ceci est un nombre pair");
+        }else {}
+        System.out.println("cice est un nombre impaire");
+    }
+}
