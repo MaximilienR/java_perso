fd16f5ce0d3c0e24295e34743eb312d028a3ec31
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2022-10-12 20:57:35 +0200

    positif of negatif ?

--- untitled1/src/request.java
+++ untitled1/src/request.java
@@ -6,9 +6,9 @@ public class request {
         System.out.println("veuillez entrer un nombre");
         Integer nbr= scan.nextInt();
         System.out.println("vous avez choisis le nombre" + " " + nbr);
-        if (nbr %2==0){
+        if (nbr%2==0){
             System.out.println("ceci est un nombre pair");
-        }else {}
+        }else
         System.out.println("cice est un nombre impaire");
     }
 }
