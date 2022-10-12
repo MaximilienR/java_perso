414914dd35234d76fa28fee3c6ecf46a123fbfad
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2022-10-12 21:20:26 +0200

    permutation

---
+++ permutation/src/main/java/org/example/Permutation.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Permutation {
+    public static void foo(){
+        int a=6;
+        int b=7;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int c;
+        c=a;
+        System.out.println(c);
+        a=b;
+        b=c;
+        System.out.println("la valeur a est égal à"+a);
+        System.out.println("la valeurs b est égale à"+b);
+        System.out.println("la valeur c est égale à)"+c);
+
+
+    }
+}
